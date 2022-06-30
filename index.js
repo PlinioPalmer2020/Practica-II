6b0e0858338b0455e308ff6f38e0b89ca4cad13b
Plinio Palmer <pliniopalmer@gmail.com>
2022-06-29 23:26:32 -0400

    la parte 6 ya hecha

--- index.js
+++ index.js
@@ -24,8 +24,35 @@ console.log(`El Area del Circulo es ${AreaCirculo(pi,5)}`)
 // 5 - Utilice de destructuring para crear dos variable que almacenen los primeros dos elementos del arreglo resultante del ejercicio 2. 
 const destructuring = ([valor1, valor2]) =>
     console.log(`Los primeros dos elemento del arreglo son ${valor1} y ${valor2} del ejercicio 2`)
-
-
 destructuring(impares);
 
 // 6 - Cree una clase llamada persona con dos atributos : nombre y apellido. Luego cree otra clase llama empleado que herede de Persona y tenga un atributo adicional llamado salario.
+
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre
+        this.apellido = apellido
+    }
+}
+
+class Empleado extends Persona{
+    constructor(nombre, apellido, salario){
+        super(nombre,apellido)
+        this.salario = salario
+    }
+}
+
+var persona1 = new Persona("Plinio","Palmer");
+var empleado1 = new Empleado("Plinio","Palmer","50,000.00");
+
+const info = Persona =>{
+    const {nombre, apellido} = Persona;
+    console.log(`La persona ${nombre} ${apellido}`);
+}
+info(persona1);
+
+const info2 = Empleado =>{
+    const {nombre, apellido, salario} = Empleado;
+    console.log(`El empleado ${nombre} ${apellido} tiene un salario de RD$${salario}`);
+}
+info2(empleado1);
